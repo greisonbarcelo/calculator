cdd9912954038a1313f2ab523236d511fb7f5ee8
Kei <barcelogreison@gmail.com>
2025-03-10 18:14:25 +0800

    Testing some functions for taking the first number

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,45 @@
-let firstNum, secondNum, operator;
-firstNum = 1;
-operator = '+'
-secondNum = 2;
+let display = document.querySelector("#display h2");
+// console.log(display.textContent);
+
+let displayValue = 0;
+display.textContent = displayValue;
+
+let buttonTest = document.querySelectorAll("button");
+// console.log(buttonTest);
+
+let firstNumber;
+
+function getFirstNumber(button) {
+    for(let i = 0; i <= 9; i++) {
+        if (button.textContent.includes(i)) {
+            return i;
+        }
+    }
+}
+buttonTest.forEach(button => {
+    button.addEventListener('click', () => {
+        if (display.textContent === '0') {
+            display.textContent = getFirstNumber(button); // Replace '0' with the new number
+        } else {
+            display.textContent += getFirstNumber(button); // Append new number
+        }
+
+        displayValue = display.textContent;
+        firstNumber = displayValue;
+        console.log(`dv = ${displayValue}`);
+        console.log(`first NUMBER ${displayValue}`)
+
+    });
+});
+
+
+
+
+
+// let firstNum, secondNum, operator;
+// firstNum = 1;
+// operator = '+'
+// secondNum = 2;
 
 function add(a, b) {
     return a + b;
@@ -28,4 +66,4 @@ function operate(operator, fNum, sNum) {
     } else return 'ERR'
 }
 
-console.log(operate(operator, firstNum, secondNum));
+// console.log(operate(operator, firstNum, secondNum));
